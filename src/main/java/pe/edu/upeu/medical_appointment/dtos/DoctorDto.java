66357b116de71f9c9e0705029ccd58561fd2ffee
Jimmy Guevara <alexjc153@gmail.com>
2026-06-20 01:25:52 -0500

    Dockerized

---
+++ src/main/java/pe/edu/upeu/medical_appointment/dtos/DoctorDto.java
@@ -0,0 +1,7 @@
+package pe.edu.upeu.medical_appointment.dtos;
+
+public record SpecialityDto() {
+    public record SpecialityRequest(String name, String description) {}
+
+    public record SpecialityResponse(Long id, String name, String description) {}
+}
---
+++ src/main/java/pe/edu/upeu/medical_appointment/dtos/DoctorScheduleDto.java
@@ -0,0 +1,4 @@
+package pe.edu.upeu.medical_appointment.dtos;
+
+public record DoctorScheduleDto() {
+}
